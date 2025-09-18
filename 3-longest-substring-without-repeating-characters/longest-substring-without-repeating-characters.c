3678e9036fad4f7f7441c06cc8563f85f61a0dbc
MANOJ IYYA <manojiyya123@gmail.com>
2025-09-18 10:45:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.13%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -1,18 +1,10 @@
 int lengthOfLongestSubstring(char* s) {
     int c[256]={0},m=0,k=0;
     for(int i=0;s[i]!='\0';i++) {
-       if( c[s[i]]==0){
-         c[s[i]]=i+1;
-        
-       }
-       else{
             k=c[s[i]]>k?c[s[i]]:k;
             c[s[i]]=i+1;
-       }
        int t=i-k+1;
         m=m<t?t:m;
     }
    return m;
 }
-
-
