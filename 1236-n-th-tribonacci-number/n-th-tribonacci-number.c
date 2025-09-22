32330d2e1f8b1d50d73919fbc72d813d5eec54e6
MANOJ IYYA <manojiyya123@gmail.com>
2025-09-22 11:58:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (62.87%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.c
@@ -0,0 +1,14 @@
+int tri(int n,int *dp){
+    if(n==0||n==1) return n;
+    if(n==2) return 1;
+    dp[0]=0;dp[1]=1;dp[2]=1;
+    for(int i=3;i<=n;i++)
+    dp[i]= dp[i-1]+dp[i-2]+dp[i-3];
+    return dp[n];
+}
+
+
+int tribonacci(int n) {
+    int dp[n+1];
+    return tri(n,dp);
+}
