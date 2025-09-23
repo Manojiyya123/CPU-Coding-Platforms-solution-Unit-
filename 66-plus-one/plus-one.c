f3ebbd23bcf7411179c31f8ae3fc20e451db9151
MANOJ IYYA <manojiyya123@gmail.com>
2025-09-23 21:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (54.24%) - LeetSync

---
+++ 66-plus-one/plus-one.c
@@ -0,0 +1,33 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int *result = malloc(sizeof(int) * (digitsSize + 1));
+    if (result == NULL)
+    {
+        *returnSize = 0;
+        return NULL;
+    }
+
+
+    memcpy(result + 1, digits, sizeof(int) * digitsSize);
+    result[0] = 0;
+    int k = digitsSize;
+
+    result[k] += 1;
+    while (result[k] == 10 && k >= 0)
+    {
+        result[k--] = 0;
+        result[k] += 1;
+    }
+    if (result[0] == 0)
+    {
+        *returnSize = digitsSize;
+        return result + 1; // Leak here
+    }
+    else
+    {
+        *returnSize = digitsSize + 1;
+        return result;
+    }
+}
