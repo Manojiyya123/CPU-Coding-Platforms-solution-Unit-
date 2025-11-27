4a20761bebab8c4c02334df946d14c5bdeaf247b
MANOJ IYYA <manojiyya123@gmail.com>
2025-11-27 11:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (95.22%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
+    struct ListNode* new=head,*l=NULL,*s=NULL,*c,*f;
+    if (head == NULL || left == right) return head;
+    int i=1;
+    while(i<left) {
+        l=new;
+        new=new->next;
+        i++;
+    }
+    f=new;
+    while(i<=right){
+        c=new;
+        new=new->next;
+        c->next=s;
+        s=c;
+        i++;
+    }
+    if (l != NULL) {
+        l->next = s;
+    } else {
+        head = s;   // left = 1 → new head
+    }
+    f->next=new;
+    return head;
+}
