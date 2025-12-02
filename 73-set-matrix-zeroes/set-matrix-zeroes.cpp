4d523cc225fb7a51715ee94fb645aa6b50bb1767
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-02 12:18:07 +0530

    Time: 2 ms (12.69%) | Memory: 19.2 MB (5.67%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
         vector<int>c,r;
+        int cr[200]={0},cc[200]={0};
+
         int m=mat.size(),n=mat[0].size();
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
                 if(mat[i][j]==0){
+                    if(cr[i]==0){
                     r.push_back(i);
+                    cr[i]=1;}
+                    if(cc[j]==0){
                     c.push_back(j);
+                    cc[j]=1;
+                    }
                 }
         for(int i=0;i<r.size();i++)
             for(int j=0;j<n;j++)
