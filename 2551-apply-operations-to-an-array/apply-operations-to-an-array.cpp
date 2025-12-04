1da8079c0c2e2ad828c64c22be78d299401225b9
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-04 11:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (88.45%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                nums[i-1]*=2;
+                nums[i]=0;
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++) 
+            if(nums[i]!=0) nums[c++]=nums[i];
+        while(c<n) nums[c++]=0;
+        return nums;
+    }
+};
