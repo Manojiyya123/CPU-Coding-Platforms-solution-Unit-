b35b5c9daa29211086ba0e34216c145c71d640da
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-05 23:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (22.63%) - LeetSync

--- 189-rotate-array/rotate-array.c
+++ 189-rotate-array/rotate-array.c
@@ -1,27 +1,24 @@
 void rotate(int* nums, int l, int k) {
     k = k % l;
     if (k == 0) return;
-    if(l>=7&&l<45){
-    int t,s=t=nums[l-k],c=nums[0];
-    int i=0;
-    while(c!=s){
-        c=nums[i];
-        nums[i]=t;
-        t=c;
-        i=(i+k)%l;
-    }
-    }
-    else{
-        int n[l]; 
-    int i = 0;
-    for (int j = l - k; j < l; j++) {
-        n[i++] = nums[j];
-    }
-    for (int j = 0; j < l - k; j++) {
-        n[i++] = nums[j];
-    }
-    for (i = 0; i < l; i++) {
-        nums[i] = n[i];
-    }
+
+    int count = 0;
+    int start = 0;
+
+    while (count < l) {
+        int cur = start;
+        int prev = nums[start];
+
+        while (1) {
+            int next = (cur + k) % l;
+            int temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            cur = next;
+            count++;
+
+            if (cur == start) break;
+        }
+        start++;
     }
 }
