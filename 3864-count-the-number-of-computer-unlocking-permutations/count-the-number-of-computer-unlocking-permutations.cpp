bb4c24c6ca0d9bde0c5d1ebdd64a3b3ef8540b11
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-10 15:40:18 +0530

    Time: 325 ms (5.00%) | Memory: 93.6 MB (19.50%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -4,8 +4,8 @@ public:
     static const int M = 1000000007;
     long long fact[N];
     Solution() {
-        fact[0] = 1;
-        for (int i = 1; i < N; i++) {
+        fact[0]= fact[1]= 1;
+        for (int i = 2; i < N; i++) {
             fact[i] = (fact[i - 1] * i) % M;
         }
     }
