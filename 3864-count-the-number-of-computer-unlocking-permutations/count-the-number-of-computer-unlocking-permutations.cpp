2f927186ad6dda90ee02dc3d831731c17a6fa2ca
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-10 15:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 92.9 MB (47.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-     static const int N = 100010;
     static const int M = 1000000007;
-    long long fact[N];
-    Solution() {
-        fact[0]= fact[1]= 1;
-        for (int i = 2; i < N; i++) {
-            fact[i] = (fact[i - 1] * i) % M;
-        }
-    }
     int countPermutations(vector<int>& com) {
         int n=com.size();
-        for(int i=1;i<n;i++)
+        long long s=1;
+        for(int i=1;i<n;i++){
             if(com[i]<=com[0]) return 0;
-        return fact[n-1];
+            s=(s*i)%M;
+        }
+        return s;
     }
 };
