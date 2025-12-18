323c3ce77f8b32c890dd6b1b50fe69054b263c70
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-18 15:58:58 +0530

    Time: 8 ms (33.51%) | Memory: 102.4 MB (27.53%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool issafe(int i,int j,int &r,int &c){
+        return(i<0||j<0||i>=r||j>=c);
+    }
+    void dfs(vector<vector<int>>&g,int &r,int &c,int i,int j,int &t){
+        if(issafe(i,j,r,c)||g[i][j]==0) {
+             t++;
+            return;}
+        if(g[i][j]==2) return;
+        g[i][j]=2;
+        dfs(g,r,c,i-1,j,t);
+        dfs(g,r,c,i+1,j,t);
+        dfs(g,r,c,i,j-1,t);
+        dfs(g,r,c,i,j+1,t);
+    }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int r=grid.size(),c=grid[0].size();
+        int m=0;
+        for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    dfs(grid,r,c,i,j,m);
+                    return m;
+                }
+            }
+        return m;
+    }
+};
