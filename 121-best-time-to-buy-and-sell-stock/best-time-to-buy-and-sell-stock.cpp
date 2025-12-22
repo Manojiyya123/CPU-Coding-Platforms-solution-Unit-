f3a48e6fa6cab317bf2bd2f0ae383f88593d7040
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-22 11:10:24 +0530

    Time: 3 ms (28.91%) | Memory: 97.9 MB (5.91%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& p) {
-        int n=p.size();
-        int j=n-1,m;
-        int g[n];g[n-1]=m=p[n-1];
-        while(j>=0){
-            if(p[j]>=m){
-                m=p[j];
-                g[j--]=m;
-            }
-            else g[j--]=m;
-        }
+        int n=p.size(),m=0;
+        int g[n];
+        for(int i=n-1;i>=0;i--)
+            g[i]=m=p[i]>m?p[i]:m;
         m=0;
-        for(int i=0;i<n;i++){
-            j=g[i]-p[i];
-            m=j>m?j:m;
-        }
+        for(int i=0;i<n;i++)
+            m=(g[i]-p[i])>m?(g[i]-p[i]):m;
         return m;
     }
 };
