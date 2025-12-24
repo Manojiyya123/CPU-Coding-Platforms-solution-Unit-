e5dc18001abd98b282222fe8044fd0f644111f14
MANOJ IYYA <manojiyya123@gmail.com>
2025-12-24 11:25:40 +0530

    Time: 4 ms (19.84%) | Memory: 29.4 MB (99.28%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.c
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -2,19 +2,19 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* productExceptSelf(int* nums, int n, int* r) {
-    int p=1,z=0;
+    int p=1,z=0,zi=0;
     for(int i=0;i<n;i++) {
         if(nums[i]!=0)p*=nums[i];
-        else z++;}
-    if(z>1)
-    for(int i=0;i<n;i++) nums[i]=0;
-    else if(z==0)
-    for(int i=0;i<n;i++) nums[i]=p/nums[i];
-    else
-    for(int i=0;i<n;i++) {
-        if(nums[i]==0) nums[i]=p;
-        else nums[i]=0;
+        else {
+            z++;zi=i;
+        }
     }
+    if(z>=1){
+        for(int i=0;i<n;i++) nums[i]=0;
+        if(z==1) nums[zi]=p;
+    }
+    else
+        for(int i=0;i<n;i++) nums[i]=p/nums[i];
     *r=n;
     return nums;
 }
