1ebfbad35b9b56e9d6dda027cd45fe4462fdf807
MANOJ IYYA <manojiyya123@gmail.com>
2026-01-03 21:23:01 +0530

    Time: 2 ms (0.00%) | Memory: 7.8 MB (0.00%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.c
@@ -0,0 +1,12 @@
+int numOfWays(int n) {
+    long long MOD = 1e9 + 7;
+        long long a = 6;
+        long long b = 6;
+        for (int i = 2; i <= n; i++) {
+            long long newA = (a * 3 + b * 2) % MOD;
+            long long newB = (a * 2 + b * 2) % MOD;
+            a = newA;
+            b = newB;
+        }
+     return (a + b) % MOD;
+}
