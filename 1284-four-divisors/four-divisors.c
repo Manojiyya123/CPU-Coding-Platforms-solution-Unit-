2dc5e95452a680f395faf1a00ff403a783e0e451
MANOJ IYYA <manojiyya123@gmail.com>
2026-01-04 22:23:38 +0530

    Time: 1 ms (95.45%) | Memory: 8.7 MB (40.91%) - LeetSync

--- 1284-four-divisors/four-divisors.c
+++ 1284-four-divisors/four-divisors.c
@@ -2,16 +2,19 @@ void divide(int n,int *s){
     if(n<6) return;
     int c=2,m=1+n;
     int t=sqrt(n);
-    for(int i=2;i<=t;i++)
+    for(int i=2;i<=t;i++){
         if(n%i==0){
-            if(n/i==i){
+            int d=n/i;
+            if(d==i){
                  m+=i;c++;
             }
             else{
-                m=m+i+n/i;
+                m=m+i+d;
                 c+=2;
             }
         }
+        if(c>4) return;
+    }
     if(c==4) *s+=m;
 }
 
