8e4cf5399a2b8e5e70a6aa081b576b0da4d3fac4
MANOJ IYYA <manojiyya123@gmail.com>
2026-01-04 22:09:36 +0530

    Time: 94 ms (36.36%) | Memory: 8.5 MB (100.00%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.c
@@ -0,0 +1,25 @@
+void divide(int n,int *s){
+    int c=2,m=1+n;
+    int t=sqrt(n);
+    for(int i=2;i<=t;i++){
+        if(n%i==0){
+            if(n/i==i){
+                 m+=i;c++;
+            }
+            else{
+                m=m+i+n/i;
+                c+=2;
+            }
+        }
+    }
+    printf("%d %d %d\n",c,m,*s);
+    if(c==4) *s+=m;
+}
+
+int sumFourDivisors(int* nums, int n) {
+    int s=0;
+    for(int i=0;i<n;i++){
+        divide(nums[i],&s);
+    }
+    return s;
+}
