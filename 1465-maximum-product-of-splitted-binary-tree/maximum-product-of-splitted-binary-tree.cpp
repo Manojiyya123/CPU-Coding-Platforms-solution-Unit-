a6faab69a67fe2fea81dd64ee63d5dabeb49d7f9
MANOJ IYYA <manojiyya123@gmail.com>
2026-01-07 21:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 78 MB (73.71%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -11,44 +11,21 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, long long> mp;
+    long long totalSum = 0;
+    long long ans = 0;
+    int M = 1e9 + 7;
 
-    long long sumt(TreeNode* r) {
-        if (r == NULL) return 0;
-
-        long long left = sumt(r->left);
-        long long right = sumt(r->right);
-
-        mp[r] = r->val + left + right;
-        return mp[r];
+    long long dfs(TreeNode* root) {
+        if (!root) return 0;
+        long long subSum = root->val + dfs(root->left) + dfs(root->right);
+        ans = max(ans, subSum * (totalSum - subSum));
+        return subSum;
     }
 
     int maxProduct(TreeNode* root) {
-        if (root == NULL) return 0;
-
-        long long tsum = sumt(root);
-        long long maxp = 0;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* t = q.front();
-            q.pop();
-            long long e=0;
-            if (t->left) {
-                q.push(t->left);
-                e = mp[t->left];
-                maxp = max(maxp, e * (tsum - e));
-            }
-            e=0;
-            if (t->right) {
-                q.push(t->right);
-                e = mp[t->right];
-                maxp = max(maxp, e * (tsum - e));
-            }
-        }
-
-        return maxp % 1000000007;
+        totalSum = dfs(root);
+        dfs(root);
+        return ans % M;
     }
 };
+
