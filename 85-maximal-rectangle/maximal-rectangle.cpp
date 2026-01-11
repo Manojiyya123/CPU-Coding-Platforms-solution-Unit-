9b44f2b2a69c3e04a9745d0fde78038837ea98ed
MANOJ IYYA <manojiyya123@gmail.com>
2026-01-11 22:13:07 +0530

    Time: 27 ms (5.92%) | Memory: 17.2 MB (92.98%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int M = matrix.size();
-        if (M == 0) return 0;
+        if (!M) return 0;
         int N = matrix[0].size();
         vector<vector<int>> mat(M, vector<int>(N,0));
         for (int i = 0; i < M; i++) 
             for (int j = 0; j < N; j++)
                 if (matrix[i][j] == '1')
                     mat[i][j] = (j ? mat[i][j - 1] : 0) + 1;
-         int ans = 0;
+        int ans = 0;
         for (int j = 0; j < N; j++)
             for (int i = 0; i < M; i++) {
                 int w = mat[i][j];
-                for (int k = i; w && k < M; k++) {
+                if(!w) continue;
+                for (int k = i;k < M; k++) {
                     w = min(w, mat[k][j]);
                     ans = max(ans, w * (k - i + 1));
                 }
