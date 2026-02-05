251535513749dbc5e9bbae34b3d9f3adb08bac1b
MANOJ IYYA <manojiyya123@gmail.com>
2026-02-05 09:52:00 +0530

    Time: 4 ms (79.67%) | Memory: 25.6 MB (70.75%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int s=nums.size();
-        vector<int> r(s,0);
+        vector<int> r;
         for(int i=0;i<s;i++){
             int x=(i+nums[i])%s;
-            if (x<0)x=s+x;
-            r[i]=nums[x];
+            x=x<0?x+s:x;
+            r.push_back(nums[x]);
         }
         return r;
     }
