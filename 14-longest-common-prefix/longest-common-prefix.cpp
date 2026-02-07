cf38bb8403379817e2a6e50cbc9a41f775e13454
MANOJ IYYA <manojiyya123@gmail.com>
2026-02-07 11:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (16.05%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,8 +5,8 @@ public:
         sort(strs.begin(),strs.end());
         string s1 = strs[0];
         string s2 = strs[strs.size()-1];
-        int n=s1.size(),m=s2.size(),i=0;
-        while(i<n&&i<m)
+        int i=0;
+        while(s1[i]!='\0'&&s2[i]!='\0')
             if(s1[i]!=s2[i]||++i<0) break;
         return s1.substr(0,i);
     }
