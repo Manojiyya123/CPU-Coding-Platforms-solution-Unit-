107afaa93dbd92a993ab7a7953e8e19d0b6bb1df
MANOJ IYYA <manojiyya123@gmail.com>
2026-02-20 22:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (72.82%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int n=h.size(),l=need.size();
         for(int i=0;i<n;i++){
             if(h[i]==need[0]&&n-i>=l){
-                int j=0,k=i;
+                int j=1,k=i+1;
                 while(j<l&&h[k++]==need[j]) j++;
                 if(j==l) return i;
             }
