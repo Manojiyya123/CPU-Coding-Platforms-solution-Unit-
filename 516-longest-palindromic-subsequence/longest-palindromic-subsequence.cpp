7137ab99aeaa71a01c12ad304551394a0a865043
MANOJ IYYA <manojiyya123@gmail.com>
2026-04-29 10:06:00 +0530

    Time: 47 ms (89.68%) | Memory: 75.9 MB (42.62%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int dp[1001][1001];
-
-    int LPS(string &s, int i, int j){
+    int LPS(string &s, int i, int j, vector<vector<int>> &dp){
         if(i > j) return 0;
         if(i == j) return 1;
         if(dp[i][j] != -1) return dp[i][j];
         if(s[i] == s[j])
-            return dp[i][j] = 2 + LPS(s, i+1, j-1);
+            return dp[i][j] = 2 + LPS(s, i+1, j-1, dp);
 
-        return dp[i][j] = max(LPS(s, i+1, j), LPS(s, i, j-1));
+        return dp[i][j] = max(LPS(s, i+1, j, dp), LPS(s, i, j-1, dp));
     }
 
     int longestPalindromeSubseq(string s) {
-        memset(dp, -1, sizeof(dp));
-        return LPS(s, 0, s.size()-1);
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return LPS(s, 0, n-1, dp);
     }
 };
